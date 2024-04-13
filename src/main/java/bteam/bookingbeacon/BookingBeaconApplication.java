42103e253aa94faff8a3adcfff229d12532b1a16
eddie727 <48875473+jjmmll0727@users.noreply.github.com>
2024-04-13 09:16:43 +0900

    Initial commit

---
+++ src/main/java/bteam/bookingbeacon/BookingBeaconApplication.java
@@ -0,0 +1,13 @@
+package bteam.bookingbeacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBeaconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBeaconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bteam/bookingbeacon/BookingBeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package bteam.bookingbeacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
