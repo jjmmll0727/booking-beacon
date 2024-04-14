53dc882c9edf5c35225e716d86f4799d24fcc631
jjmmll0727 <jjmmll0727@gmail.com>
2024-04-14 17:22:48 +0900

    feat: JPA setting

--- src/main/java/bteam/bookingbeacon/BookingBeaconApplication.java
+++ src/main/java/bteam/bookingbeacon/BookingBeaconApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableAutoConfiguration, @ComponentScan, @Configuration 를 하나로 묶은 어노테이션
 public class BookingBeaconApplication {
 
 	public static void main(String[] args) {
